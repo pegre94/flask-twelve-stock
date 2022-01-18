f8623e3698e20b2f4463c51c8a523dbf64dc5429
Przemyslaw Grenda <przemekgrenda@gmail.com>
2022-01-18 15:52:22 +0100

    Calculating cagr compound endpoint

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from sqlalchemy import select
 import pandas as pd
 from flask_marshmallow import Marshmallow
 import os
+import math
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+
 
 # Init app
 app = Flask(__name__)
@@ -94,17 +98,55 @@ def get_products():
     return jsonify(results)
 
 
-def get_products_with_symbol(symbol):
-    currencies_exchange_symbol = {"NOK": "NOK/USD", "EUR": "EUR/USD"}
-    # get request args
-    currency = request.args.get("currency", default="USD")
-    # get results
-    stocks_with_symbol = Stocks.query.filter(Stocks.symbol == symbol)
-    if not currency == "USD":
-        exchange_rate = get_exchange_rate(currencies_exchange_symbol[currency])
-    return jsonify([results[0], exchange_rate[0]])
-    # return currency
-    # return jsonify(exchange_results)
+@app.route("/cagr", methods=["GET"])
+def calculate_cagr():
+    symbol = request.args.get("symbol")
+    start_date = request.args.get("start_date")
+    num_of_years = int(request.args.get("num_of_years"))
+
+    def _find_closest_to_date(basic_query, model, ts):
+        import math
+
+        # first check if row with ts exists
+        if same_date := basic_query.filter(model.datetime == ts).first():
+            return same_date
+        # find the closest events which are greater/less than the timestamp
+        gt_event = (
+            basic_query.filter(model.datetime > ts)
+            .order_by(model.datetime.asc())
+            .first()
+        )
+        lt_event = (
+            basic_query.filter(model.datetime < ts)
+            .order_by(model.datetime.desc())
+            .first()
+        )
+
+        # find diff between events and the ts, if no event found default to infintiy
+        gt_diff = (gt_event.datetime - ts).total_seconds() if gt_event else math.inf
+        lt_diff = (ts - lt_event.datetime).total_seconds() if lt_event else math.inf
+
+        # return the newest event if it is closer to ts else older event
+        # if an event is None its diff will always be greater as we set it to infinity
+        return gt_event if gt_diff < lt_diff else lt_event
+
+    start_date = parse(start_date)
+    stocks = Stocks.query.filter(Stocks.symbol == symbol)
+    first_stock = _find_closest_to_date(stocks, Stocks, start_date)
+    end_time = first_stock.datetime + relativedelta(years=num_of_years)
+    last_stock = _find_closest_to_date(stocks, Stocks, end_time)
+    cagr = (last_stock.close / first_stock.open) ** (1 / float(num_of_years)) - 1
+    cagr_perc = round(cagr * 100, 2)
+
+    return jsonify(
+        {
+            "start_date": first_stock.datetime,
+            "end_date": last_stock.datetime,
+            "first_value": first_stock.open,
+            "last_value": last_stock.close,
+            "CAGR": cagr_perc,
+        }
+    )
 
 
 @app.route("/tests/")
