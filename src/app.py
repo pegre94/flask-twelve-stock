188ea0a0c97bb4d500e19661eeb7ca06ab46a3f0
Przemyslaw Grenda <przemekgrenda@gmail.com>
2022-01-21 09:07:25 +0100

    Add Sharpe Ratio endpoin

--- src/app.py
+++ src/app.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select
+import pandas_datareader as pdr
+import numpy as np
 
 # Init app
 app = Flask(__name__)
@@ -68,6 +70,28 @@ class CurrenciesSchema(ma.SQLAlchemyAutoSchema):
         model = Currencies
 
 
+def find_closest_to_date(basic_query, model, ts):
+    import math
+
+    # first check if row with ts exists
+    if same_date := basic_query.filter(model.datetime == ts).first():
+        return same_date
+    # find the closest events which are greater/less than the timestamp
+    gt_event = (
+        basic_query.filter(model.datetime > ts).order_by(model.datetime.asc()).first()
+    )
+    lt_event = (
+        basic_query.filter(model.datetime < ts).order_by(model.datetime.desc()).first()
+    )
+    # find diff between events and the ts, if no event found default to infintiy
+    gt_diff = (gt_event.datetime - ts).total_seconds() if gt_event else math.inf
+    lt_diff = (ts - lt_event.datetime).total_seconds() if lt_event else math.inf
+
+    # return the newest event if it is closer to ts else older event
+    # if an event is None its diff will always be greater as we set it to infinity
+    return gt_event if gt_diff < lt_diff else lt_event
+
+
 @app.route("/stocks/", methods=["GET"])
 def get_stocks():
     symbol = request.args.get("symbol")
@@ -121,33 +145,6 @@ def get_stocks():
 def calculate_cagr():
     symbol = request.args.get("symbol")
     num_of_years = request.args.get("num_of_years")
-
-    def _find_closest_to_date(basic_query, model, ts):
-        import math
-
-        # first check if row with ts exists
-        if same_date := basic_query.filter(model.datetime == ts).first():
-            return same_date
-        # find the closest events which are greater/less than the timestamp
-        gt_event = (
-            basic_query.filter(model.datetime > ts)
-            .order_by(model.datetime.asc())
-            .first()
-        )
-        lt_event = (
-            basic_query.filter(model.datetime < ts)
-            .order_by(model.datetime.desc())
-            .first()
-        )
-
-        # find diff between events and the ts, if no event found default to infintiy
-        gt_diff = (gt_event.datetime - ts).total_seconds() if gt_event else math.inf
-        lt_diff = (ts - lt_event.datetime).total_seconds() if lt_event else math.inf
-
-        # return the newest event if it is closer to ts else older event
-        # if an event is None its diff will always be greater as we set it to infinity
-        return gt_event if gt_diff < lt_diff else lt_event
-
     end_date = datetime.now()
 
     def _get_cagr(stock, num_of_years):
@@ -156,9 +153,9 @@ def calculate_cagr():
         else:
             num_of_years = int(num_of_years)
 
-        last_stock = _find_closest_to_date(stocks, Stocks, end_date)
+        last_stock = find_closest_to_date(stocks, Stocks, end_date)
         start_time = last_stock.datetime - relativedelta(years=num_of_years)
-        first_stock = _find_closest_to_date(stocks, Stocks, start_time)
+        first_stock = find_closest_to_date(stocks, Stocks, start_time)
         cagr = (last_stock.close / first_stock.open) ** (1 / float(num_of_years)) - 1
         cagr_perc = round(cagr * 100, 2)
         return {
@@ -188,24 +185,52 @@ def calculate_cagr():
     return jsonify(results)
 
 
-@app.route("/tests/")
-def pandas_test():
-    stock_stmt = select(Stocks).where(Stocks.symbol == "F")
-    stock = pd.read_sql(stock_stmt, db.engine)
-    exchange_stmt = select(Currencies).where(Currencies.symbol == "EUR/USD")
-    exchange = pd.read_sql(exchange_stmt, db.engine)
-    #     symbol = db.Column(db.String(255))
-    # datetime = db.Column(db.DateTime)
-    # open = db.Column(db.Float)
-    # high = db.Column(db.Float)
-    # low = db.Column(db.Float)
-    # close = db.Column(db.Float)
-    # volume = db.Column(db.Float)
-    # pd.DataFrame(
-    #     stock.values * exchange.values, columns=stock.columns, index=stock.index
-    # )
-    exchange = currencies_schema.dump(exchange.to_json)
-    return jsonify(exchange)
+@app.route("/sharpe/", methods=["GET"])
+def get_sharpe():
+
+    req_symbol = request.args.get("symbol")
+    # num_of_years = request.args.get("num_of_years")
+    symbols = list(req_symbol) if req_symbol else CAR_PRODUCERS.keys()
+    start = datetime(2014, 2, 24)
+    stop = datetime(2022, 1, 14)
+    sp = pdr.get_data_yahoo("^GSPC", start, stop)["Adj Close"]
+
+    # calculate daily sp500 returns
+    # calculate the percentage change in value from one day to the next
+    sp_returns = sp.pct_change()
+
+    results = {}
+    for symbol in symbols:
+        # if num_of_years == "all":
+        #     num_of_years = relativedelta(stocks[0].datetime, stocks[-1].datetime).years
+        # else:
+        #     num_of_years = int(num_of_years)
+
+        stock_stmt = select(Stocks).where(Stocks.symbol == symbol)
+        stock = pd.read_sql(stock_stmt, db.engine, index_col="datetime")["close"]
+
+        # calculate daily stock_data returns
+        stock_returns = stock.pct_change()
+
+        # calculate the difference in daily returns
+        # relative performance of stocks vs. the S&P 500 benchmark.
+        excess_returns = stock_returns.sub(sp_returns, axis=0)
+
+        # calculate the mean of excess_returns
+        # how much more or less the investment yields per day compared to the benchmark
+        avg_excess_return = excess_returns.mean(skipna=True)
+        # calculate the standard deviations
+        # this shows us the amount of risk an investment in the stocks implies as compared to an investment in the S&P 500
+        sd_excess_return = excess_returns.std()
+        # calculate the daily sharpe ratio
+        daily_sharpe_ratio = avg_excess_return / sd_excess_return
+
+        # annualize the sharpe ratio
+        annual_factor = np.sqrt(252)
+        annual_sharpe_ratio = daily_sharpe_ratio * annual_factor
+        results[symbol] = round(annual_sharpe_ratio, 3)
+
+    return jsonify(results)
 
 
 # Init schema
